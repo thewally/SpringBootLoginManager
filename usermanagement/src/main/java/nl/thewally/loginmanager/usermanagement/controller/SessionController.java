65c56340c90aa24a685e95cc7c945b7cb2882bb7
arjen <arjen@detesters.nl>
2018-04-30 10:20:19 +0200

    Removed unneeded imports

--- usermanagement/src/main/java/nl/thewally/loginmanager/usermanagement/controller/SessionController.java
+++ usermanagement/src/main/java/nl/thewally/loginmanager/usermanagement/controller/SessionController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Random;
 
 @RestController
