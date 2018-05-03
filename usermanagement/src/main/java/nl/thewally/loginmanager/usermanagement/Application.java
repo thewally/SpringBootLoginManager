02d38807b408051f0c4949ab68175635ae642417
arjen <arjen@detesters.nl>
2018-05-03 07:57:46 +0200

    Added CORS Configurer

--- usermanagement/src/main/java/nl/thewally/loginmanager/usermanagement/Application.java
+++ usermanagement/src/main/java/nl/thewally/loginmanager/usermanagement/Application.java
@@ -3,6 +3,10 @@ package nl.thewally.loginmanager.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EntityScan(basePackages = {
@@ -13,5 +17,16 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:4200", "http://localhost");
+				registry.addMapping("/getUsers/*").allowedOrigins("http://localhost:4200", "http://localhost");
+			}
+		};
 	}
 }
